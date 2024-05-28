6130b86df1c14cecf6cd0d201849d8bfd06a2b5a
Juri Kasai <125288937+puplove18@users.noreply.github.com>
2024-05-28 18:31:27 +0200

    Create printer.c

---
+++ week_4/printer.c
@@ -0,0 +1,12 @@
+/*
+Write a function that prints the contents of a given int array. Remember to also pass the size of the array
+to the function. Test your function.
+  */
+
+#include <stdio/h>
+void print_int_array (int array[], int size) {
+  for (int i = 0; i < size; i++) {
+    pintf("%d", test[i]);
+  }
+  printf("\n");
+}
